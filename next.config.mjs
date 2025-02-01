ccaaff7ba246e472e345e3c0fd0fd152357580b4
caj00017 <caj00017@mix.wvu.edu>
2025-01-31 22:41:01 -0500

    gh-pages build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",
+    basePath: "/cjones-site", 
+    assetPrefix: "/cjones-site/",
+  };
+  
+  module.exports = nextConfig;
+  
